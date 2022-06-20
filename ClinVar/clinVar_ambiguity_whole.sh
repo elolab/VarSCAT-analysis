1369587fc4faab41e0cf4efb4cdb56c1cd51c694
KingN <wangningqd@outlook.com>
2022-06-20 12:11:55 +0300

    Add files via upload

---
+++ ClinVar/clinVar_ambiguity_whole.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH --cpus-per-task 1
+#SBATCH --partition normal
+#SBATCH --output ClinVar_12_1.ambiguity_whole.output.txt
+#SBATCH --error ClinVar_12_1.ambiguity_whole.Errors.txt
+#SBATCH --job-name Clin_whole
+
+python3 VarSCAT.py -A --vcf clinvar_20220109_no_scaffold.vcf --reference Homo_sapiens.GRCh38.dna.primary_assembly.fa --LRP 1 --HGVS 1 --flank 1 --adjacent 1--output ambiguity_ClinVar_whole
