56217f15579d6e81583114ecc4d5ee2074b418f7
KingN <wangningqd@outlook.com>
2022-08-25 11:17:40 +0300

    Delete 1000GP_analysis/01_data_process/chr17 directory

--- 1000GP_analysis/01_data_process/chr17/No_GP_VCF_index_chr16.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition normal
-#SBATCH --job-name N16
-/bcftools-1.5/bcftools view -G -l 5 ALL.chr16.shapeit2_integrated_snvindels_v2a_27022019.GRCh38.phased.vcf.gz > 1000GP_chr16_no_genotype.vcf.gz
-/samtools-1.5/htslib-1.5/tabix -p vcf 1000GP_chr16_no_genotype.vcf.gz
--- 1000GP_analysis/01_data_process/chr17/parallel_chr16_extract_sample_GP.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 48
-#SBATCH --partition long
-#SBATCH --job-name parallel_chr16
-cat chr16_extract_sample_GP.txt | /parallel-20211222/src/parallel -P 48 {}
