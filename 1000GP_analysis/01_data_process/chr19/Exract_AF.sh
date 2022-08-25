43ff5f6563a4a89812edad3337a72203cf33e700
KingN <wangningqd@outlook.com>
2022-08-25 11:18:56 +0300

    Add files via upload

---
+++ 1000GP_analysis/01_data_process/chr19/Exract_AF.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH --cpus-per-task 24
+#SBATCH --partition normal
+#SBATCH --job-name Extract_AF_19
+source /myenv/bin/activate
+cat 19_index.txt | /parallel-20211222/src/parallel -P 24 python3 /01_data_process/extract_INFO_GP.py --vcf 1000GP_chr19_no_genotype.vcf.gz --location {} --output {}
