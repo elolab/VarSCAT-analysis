56ac5aeeb999f81ce22466f0aa25dad571c9f1c9
KingN <wangningqd@outlook.com>
2023-06-23 18:54:37 +0300

    Add files via upload

---
+++ 1000GP_analysis/01_data_process/chr15/VarSCAT_chr15_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=1000_15
+#SBATCH --account=project
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=12G
+#SBATCH --partition=small
+
+module load tykky
+export PATH="/VarSCAT/bin:$PATH"
+
+python VarSCAT.py -T --vcf 1000GP_chr15_no_genotype.vcf.gz --reference Homo_sapiens.GRCh38.dna.primary_assembly.fa --output chr15_1000
