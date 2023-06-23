9dd047dd85722b84fb93240e86fd1f0d49731c54
KingN <wangningqd@outlook.com>
2023-06-24 01:07:14 +0300

    Delete run_combine.sh

--- 1000GP_analysis/02_data_analysis/03_TR_proportion/Results/run_combine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 10
-#SBATCH --partition normal
-#SBATCH --time 12:00:00
-#SBATCH --output gp.output.txt
-#SBATCH --error gp.Errors.txt
-#SBATCH --job-name GP
-module add R
-cat command_line.txt | /parallel-20211222/src/parallel -P 10 {}
