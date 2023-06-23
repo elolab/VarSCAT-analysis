ac188b56b9c1a591a8b70d0607f27cfd90e3c347
KingN <wangningqd@outlook.com>
2023-06-24 01:15:03 +0300

    Delete Platinum_GIAB_analysis/Platinum/NA12878 directory

--- Platinum_GIAB_analysis/Platinum/NA12878/chr10_VarSCAT_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition long
-#SBATCH --job-name NA12878_10
-source /myenv/bin/activate
-python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf NA12878.chr10.vcf --reference /Homo_sapiens/UCSC/hg38/Sequence/BWAIndex/genome.fa --similarity 100 --gap_tolerate 0 --output VarSCAT_chr10.txt
