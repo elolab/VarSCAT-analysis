fe9f9bba774871f4f00a2b5278afb7d1da08d16c
KingN <wangningqd@outlook.com>
2023-06-24 01:12:01 +0300

    Add files via upload

---
+++ Platinum_GIAB_analysis/GIAB/VarSCAT_GIAB_HG004.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=HG004
+#SBATCH --account=project
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=12G
+#SBATCH --partition=small
+
+module load tykky
+export PATH="/VarSCAT/bin:$PATH"
+
+python VarSCAT.py -T --vcf HG004_GRCh38_1_22_v4.2.1_benchmark.vcf.gz --reference GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta --output HG004
