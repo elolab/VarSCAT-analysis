73d034b983ea0e63720b985be348fda1deadcbef
KingN <wangningqd@outlook.com>
2023-06-24 01:12:39 +0300

    Delete Platinum_GIAB_analysis/GIAB/HG004 directory

--- Platinum_GIAB_analysis/GIAB/HG004/chr14_VarSCAT_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition long
-#SBATCH --job-name HG004_14
-source /myenv/bin/activate
-python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf HG004_GRCh38_1_22_v4.2.1_benchmark.chr14.vcf --reference /GIAB/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta --similarity 100 --gap_tolerate 0 --output VarSCAT_chr14.txt
