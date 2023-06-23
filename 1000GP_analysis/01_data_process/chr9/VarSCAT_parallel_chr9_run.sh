1104d12de8217c34dd3e1883bb8f5baf9aeb4394
KingN <wangningqd@outlook.com>
2023-06-23 18:51:26 +0300

    Delete VarSCAT_parallel_chr9_run.sh

--- 1000GP_analysis/01_data_process/chr9/VarSCAT_parallel_chr9_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 24
-#SBATCH --partition long
-#SBATCH --job-name gnu_9
-source /myenv/bin/activate
-time cat 9_index.txt | /parallel-20211222/src/parallel -P 24 python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf 1000GP_chr9_no_genotype.vcf.gz --reference /GRCh38/Sequence/WholeGenomeFasta/Homo_sapiens.GRCh38.dna.primary_assembly.fa --location {} --output {}
