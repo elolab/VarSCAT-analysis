87f776575246bdc7190e15d205efac20a267797c
KingN <wangningqd@outlook.com>
2023-06-24 01:14:55 +0300

    Delete Platinum_GIAB_analysis/Platinum/NA12877 directory

--- Platinum_GIAB_analysis/Platinum/NA12877/chr15_VarSCAT_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#SBATCH --cpus-per-task 1
-#SBATCH --partition long
-#SBATCH --job-name NA12877_15
-source /myenv/bin/activate
-python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf NA12877.chr15.vcf --reference /Homo_sapiens/UCSC/hg38/Sequence/BWAIndex/genome.fa --similarity 100 --gap_tolerate 0 --output VarSCAT_chr15.txt
