8acc903f991d75b58f9cccc0903bca03d5164fff
KingN <wangningqd@outlook.com>
2023-06-24 11:07:04 +0300

    Add files via upload

---
+++ Platinum_GIAB_analysis/Platinum/NA12877/chr3_VarSCAT_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH --cpus-per-task 1
+#SBATCH --partition long
+#SBATCH --job-name NA12877_3
+source /myenv/bin/activate
+python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf NA12877.chr3.vcf --reference /Homo_sapiens/UCSC/hg38/Sequence/BWAIndex/genome.fa --similarity 100 --gap_tolerate 0 --output VarSCAT_chr3.txt
