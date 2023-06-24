b7c58c6e123b27fda63dc6f6704889b8ec407f63
KingN <wangningqd@outlook.com>
2023-06-24 11:03:58 +0300

    Add files via upload

---
+++ Platinum_GIAB_analysis/GIAB/HG006/chr15_VarSCAT_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH --cpus-per-task 1
+#SBATCH --partition long
+#SBATCH --job-name HG006_15
+source /myenv/bin/activate
+python3 /VarSCAT/VarSCAT.py -A --LRP 1 --HGVS 1 --adjacent 1 -T --vcf HG006_GRCh38_1_22_v4.2.1_benchmark.chr15.vcf --reference /GIAB/GCA_000001405.15_GRCh38_no_alt_analysis_set.fasta --similarity 100 --gap_tolerate 0 --output VarSCAT_chr15.txt
